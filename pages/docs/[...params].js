ffa5293d6817b1480d34bd991b43fbf5086109ce
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-01-31 11:23:17 +0430

    with catch all routes we can make infinite routes based on the websites routs that may be a documentation site

---
+++ pages/docs/[...params].js
@@ -0,0 +1,18 @@
+import { useRouter } from "next/router"
+
+const Doc = () => {
+    const router = useRouter()
+    const { params = []} = router.query
+    console.log(params)
+
+    if (params.length === 2) {
+        return <h1>Viewing docs for feature {params[0]} and concpet {params[1]}</h1>
+    } else if (params.length === 1) {
+        return<h1>Viewing docs for feature {params[0]}</h1>
+    }
+  return (
+    <div>Docs Homepage</div>
+  )
+}
+
+export default Doc
