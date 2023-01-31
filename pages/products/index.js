1ae2567962889914a373b90de346ad66927cf6d9
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-01-31 12:27:25 +0430

    the replace prop in link replaces the current history state instead of adding a new url to the stack

--- pages/products/index.js
+++ pages/products/index.js
@@ -7,7 +7,7 @@ const ProductList = ({ productId = 100}) => {
         <Link href='/'><button>Home</button></Link>
             <h2><Link href='/products/1'>Product 1</Link></h2>
             <h2><Link href='/products/2'>Product 2</Link></h2>
-            <h2><Link href='/products/3'>Product 3</Link></h2>
+            <h2><Link href='/products/3' replace>Product 3</Link></h2>
             <h2><Link href={`/products/${productId}`}>Product {productId}</Link></h2>
         </>
     )
