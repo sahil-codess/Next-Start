b273e0a906ed6e8f12d67b24c020066958e41841
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-02-03 14:29:03 +0430

    fetching list of paths for a dynamic page, the hardcoded way is not ideal for real world project

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -12,18 +12,29 @@ const Post = ({ post }) => {
 export default Post
 
 export async function getStaticPaths() {
-    return {
-        paths: [
-            {
-                params: { postId: '1' }
-            },
-            {
-                params: { postId: '2' }
-            },
-            {
-                params: { postId: '3' }
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const data = await response.json()
+
+    const paths = data.map((post) => {
+        return {
+            params: {
+                postId: `${post.id}`
             }
-        ],
+        }
+    })
+    return {
+        // paths: [
+        //     {
+        //         params: { postId: '1' }
+        //     },
+        //     {
+        //         params: { postId: '2' }
+        //     },
+        //     {
+        //         params: { postId: '3' }
+        //     }
+        // ],
+        paths,
         fallback: false
     }
 }
