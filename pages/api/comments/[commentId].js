396376dde717a5edfab39ffcbf5b83186d87e082
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-02-06 13:54:10 +0430

    deleting a comment but i faced an error and I will solve it after

--- pages/api/comments/[commentId].js
+++ pages/api/comments/[commentId].js
@@ -1,7 +1,18 @@
-import { comments } from "@/data/comments"
+import { comments } from "@/data/comments";
 
 export default function handler(req, res) {
-    const { commentId } = req.query
-    const comment = comments.find((comment) => comment.id === parseInt(commentId))
-    res.status(200).json(comment)
+  const { commentId } = req.query;
+  if (req.method === "GET") {
+    const comment = comments.find(
+      (comment) => comment.id === parseInt(commentId)
+    );
+    res.status(200).json(comment);
+  } else if (req.method === "DELETE") {
+    const deletedComment = comments.find(
+      (comment) => comment.id === parseInt(commentId)
+    );
+    const index = findIndex((comment) => comment.id === parseInt(commentId));
+    comments.splice(index, 1);
+    res.status(200).json(deletedComment);
+  }
 }
