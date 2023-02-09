77dcde04da2e4cf28f539b0d3d3482b7d1605f9a
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-02-09 21:10:11 +0430

    did the client side authentication

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,9 @@
 import Link from "next/link";
-import { signIn, signOut } from 'next-auth/react'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 const Navbar = () => {
+    const { data: session, loading } = useSession()
+    console.log({ session, loading });
     return (
         <nav className="nav navbar-expand navbar-light bg-light">
             <h1 className=""><Link href='/' className="nav-link">NextAuth</Link></h1>
@@ -15,18 +17,23 @@ const Navbar = () => {
                 <li className="nav-item ">
                     <Link href='/blog' className="nav-link">Blog</Link>
                 </li>
-                <li className="nav-item ">
-                    <Link href='/api/auth/signin' onClick={e => {
-                        e.preventDefault()
-                        signIn('github')
-                    }} className="nav-link">Sign In</Link>
-                </li>
-                <li className="nav-item ">
-                    <Link href='/api/auth/signout' onClick={e => {
-                        e.preventDefault()
-                        signOut()
-                    }} className="nav-link">Sign Out</Link>
-                </li>
+                {
+                    !loading && !session && (<li className="nav-item ">
+                        <Link href='/api/auth/signin' onClick={e => {
+                            e.preventDefault()
+                            signIn('github')
+                        }} className="nav-link">Sign In</Link>
+                    </li>)
+                }
+                {
+                    session && (<li className="nav-item ">
+                        <Link href='/api/auth/signout' onClick={e => {
+                            e.preventDefault()
+                            signOut()
+                        }} className="nav-link">Sign Out</Link>
+                    </li>)
+                }
+
             </ul>
         </nav>
     )
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,9 @@
 import Footer from "@/components/Footer";
-import Header from "@/components/Header";
 import "@/styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import layout from "../styles/layout.css";
 import Head from "next/head";
 import Navbar from "@/components/Navbar";
+import { SessionProvider } from "next-auth/react"
 
 export default function App({ Component, pageProps }) {
   if (Component.getLayout) {
@@ -12,13 +11,15 @@ export default function App({ Component, pageProps }) {
   }
   return (
     <>
-      <Head>
-        <title>Sahilkhan Ahmadzai</title>
-        <meta name="description" content="About Page for Next Start" />
-      </Head>
-      <Navbar />
-      <Component {...pageProps} />
-      <Footer />
+      <SessionProvider>
+        <Head>
+          <title>Sahilkhan Ahmadzai</title>
+          <meta name="description" content="About Page for Next Start" />
+        </Head>
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+      </SessionProvider>
     </>
   );
 }
